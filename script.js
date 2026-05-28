bd1ecb5f1f2ce0c9fa5cb665126beb7180366050
Fernanda Lemos <fernandalemos979@gmail.com>
2026-05-28 00:04:37 -0300

    Projeto final pronto

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const botoes = document.querySelectorAll("button");
+function abrirFormulario(vaga) {
+    document.getElementById("formulario").classList.remove("escondido");
+    document.getElementById("vaga").value = vaga;
 
-botoes.forEach(botao => {
-    botao.addEventListener("click", () => {
-        alert("Candidatura enviada com sucesso!");
-    });
-});
+    // janela ao clicar em candidatar-se
+    alert("Candidatura enviada, boa sorte 🍀");
+}
+
+function cadastrar() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+
+    if (nome === "" || email === "" || telefone === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    // janela ao clicar em cadastrar
+    alert("Cadastro realizado com sucesso ✔");
+
+    // fecha formulário (opcional, deixa mais limpo)
+    document.getElementById("formulario").classList.add("escondido");
+
+    // limpa campos
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("telefone").value = "";
+}
