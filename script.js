03b3198dfb26c14f3fbe2d37447cc48515900ec5
Fernanda Lemos <fernandalemos979@gmail.com>
2026-05-28 22:13:50 -0300

    ajuste final do sistema

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-function selecionarVaga(vaga) {
-    document.getElementById("vaga").value = vaga;
-    alert("Vaga selecionada: " + vaga);
+function entrar() {
+const email = document.getElementById("emailLogin").value;
+const senha = document.getElementById("senhaLogin").value;
+
+if(email === "" || senha === "") {
+alert("Preencha login!");
+return;
 }
 
-function cadastrar() {
-    const nome = document.getElementById("nome").value;
+document.getElementById("auth").classList.add("escondido");
+document.getElementById("sistema").classList.remove("escondido");
+}
 
-    if (nome === "") {
-        alert("Preencha seus dados!");
-        return;
-    }
+function cadastrarUsuario() {
+const nome = document.getElementById("nomeCadastro").value;
 
-    document.getElementById("status").classList.remove("escondido");
+if(nome === "") {
+alert("Preencha cadastro!");
+return;
+}
 
-    alert("Candidatura enviada com sucesso! Boa sorte 🍀");
+alert("Cadastro realizado com sucesso!");
+}
 
-    document.getElementById("nome").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("telefone").value = "";
+/* VER DETALHES */
+function verDetalhes(tipo) {
+
+document.getElementById("detalhes").classList.remove("escondido");
+
+if(tipo === "adm") {
+document.getElementById("tituloVaga").innerText = "Assistente Administrativo";
+document.getElementById("descricao").innerText = "Suporte administrativo e organização de documentos.";
+document.getElementById("local").innerText = "📍 Fortaleza - CE";
+}
+
+if(tipo === "suporte") {
+document.getElementById("tituloVaga").innerText = "Suporte Técnico";
+document.getElementById("descricao").innerText = "Atendimento técnico e suporte a sistemas.";
+document.getElementById("local").innerText = "📍 Remoto / Híbrido";
+}
+}
+
+/* CANDIDATURA */
+function candidatar() {
+
+document.getElementById("status").classList.remove("escondido");
+
+alert("Candidatura enviada com sucesso! Boa sorte 🍀");
 }
